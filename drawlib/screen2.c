8dc47bc29addf28e7cfa4d18532edde994718c66
Gustavo Ficher <gustavoficher@hotmail.com>
2019-05-20 17:05:31 -0300

    Add guide commends to screen lib

--- drawlib/screen2.c
+++ drawlib/screen2.c
@@ -1,13 +1,30 @@
 #include "screen.h"
 
+
+// DEFINED WORDS:
+//
+// #define COLOR_RED (short int) 0xF800
+// #define COLOR_BLUE (short int) 0x1F
+// #define COLOR_GREEN (short int) 0x7E0
+// #define COLOR_BLACK (short int) 0x0
+// #define COLOR_WHITE (short int) 0xFFFF
+//
+// typedef short int COLOR;
+//
+// #define SCREEN_WIDTH 320
+// #define SCREEN_MAX_WIDTH 319
+// #define SCREEN_HEIGHT 240
+// #define SCREEN_MAX_HEIGHT 239
+
+
 void initScreen() {
     // prepare screen to print the game
 }
 
 void drawPixel(int x, int y, COLOR color) {
     // draw a pixel in x, y position with a color
-    // x in [0, SCREEN_MAX_WIDTH] (SCREEN_WIDTH is also defined)
-    // y in [0, SCREEN_MAX_HEIGHT] (SCREEN_HEIGHT is also defined)
+    // x in [0, SCREEN_MAX_WIDTH]
+    // y in [0, SCREEN_MAX_HEIGHT]
 }
 
 void waitScreen() {
